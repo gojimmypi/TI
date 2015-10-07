8096e03d87fe8bbe932c8dc82c50680acfdb04ac
gojimmypi <gojimmypi@users.noreply.github.com>
2015-10-07 14:28:06 -0700

    Update main.c

--- Blink/main.c
+++ Blink/main.c
@@ -10,12 +10,12 @@
 int x = 0;
 int y = 0;
 int z = 0;
-<<<<<<< HEAD
+
 int z2 = 1; // new edit 10/7
 int z3 = 0; // thsi is a CCS Work Branch Edit!
-=======
-int z2 = 1; // new
->>>>>>> master
+
+int z2 = 2; // web edit
+
 
 main() // All C programs have a main function
 {
